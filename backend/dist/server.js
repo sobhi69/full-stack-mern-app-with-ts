c25602870b0f4884cbff72caa7735e853759a8d7
sobhi69 <mhegazy6371@gmail.com>
2025-05-09 22:41:27 +0300

    add cors headers

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -9,27 +9,32 @@ dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const connectToDb_1 = require("./lib/connectToDb");
 const cors_1 = __importDefault(require("cors"));
-const corsOptions_1 = require("./lib/corsOptions");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const path_1 = __importDefault(require("path"));
 const __dirName = path_1.default.resolve();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3001;
-// middlewares
-app.use((0, cors_1.default)(corsOptions_1.corsOptions));
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: false }));
-app.use((0, cookie_parser_1.default)());
 const user_1 = __importDefault(require("./routes/user"));
 const client_1 = __importDefault(require("./routes/client"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const item_1 = __importDefault(require("./routes/item"));
 const sale_1 = __importDefault(require("./routes/sale"));
-// routes
+// middlewares
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header("Access-Control-Allow-Origin", '*');
     next();
 });
+app.use((0, cors_1.default)({
+    origin(requestOrigin, callback) {
+        console.log(requestOrigin);
+        return callback(null, true);
+    }, optionsSuccessStatus: 200, credentials: true
+}));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, cookie_parser_1.default)());
+// routes
 app.use('/api/user', user_1.default);
 app.use('/api/client', client_1.default);
 app.use('/api/auth', auth_1.default);
