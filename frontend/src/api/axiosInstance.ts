eeaf0bcb2ed048fc28741dc9e943fe407e3baacf
sobhi69 <mhegazy6371@gmail.com>
2025-05-22 19:46:20 +0300

    base url

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE == 'production' ? "https://backend-easy-production.up.railway.app/api" : 
-    "http://localhost:3011/api"
+    baseURL: import.meta.env.MODE == 'production' ? "https://backend-easy-to-earn-production.up.railway.app/api" :
+        "http://localhost:3011/api"
 })
